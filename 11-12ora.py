ff77c9af1d5918ee82c9e62c5e31ac83abc78851
kaulicsb <112930606+kaulicsb@users.noreply.github.com>
2022-10-11 09:39:26 +0200

    Add files via upload

---
+++ 11-12ora.py
@@ -0,0 +1,30 @@
+#feladat 1
+"""
+gondolt_szam = 4
+tipp  = input("gondoltam egy szamra tippeld meg")
+tipp = int(tipp)
+if tipp == gondolt_szam:
+    print("ügyesvagy")
+    print("ez az igaz ág")
+print("pápá")
+
+else:
+    print("hosszan gondolkodtal rajta?")
+    print("nem erte meg")
+print("pápá")
+"""
+
+#feladat 2
+szam1 = input('Kérek egy számot')
+szam1 = int(szam1)
+szam2 = input('Kérek egy másik számot')
+szam2 = int(szam2)
+if szam1 == szam2:
+    print('A két szám megegyezik')
+if szam1 != szam2:
+    print('A két szám nem egyezik meg')
+print('A program vége')
+if szam1 < szam2:
+    print('Az első szám kisebb, mint a második')
+else:
+    print('Az első szám nagyobb vagy egyenlő a második számnál')
