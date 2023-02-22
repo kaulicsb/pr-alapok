756e52a3256c0ae171311eb09ed298ff182e9ee3
kaulicsb <112930606+kaulicsb@users.noreply.github.com>
2023-02-22 07:40:27 +0100

    Add files via upload

---
+++ feladat_3.py
@@ -0,0 +1,10 @@
+#Kaulics Balázs 10c 3.feladat
+
+honapok = ['január', 'február','március', 'április', 'május', 'június'] 
+  
+index = 0
+for x in honapok:
+    print(index , x)
+    index = index + 1
+print("A program vége!")
+  
